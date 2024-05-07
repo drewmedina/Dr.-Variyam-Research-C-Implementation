b356b103489105b95d2ccd35a08739e4a0550bf0
drewmedina <ofc10drew@gmail.com>
2024-05-07 11:11:37 -0500

    push

--- cvm_estimates.java
+++ cvm_estimates.java
@@ -1,46 +1,94 @@
+<<<<<<< Updated upstream
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+=======
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+import static java.awt.SystemColor.info;
+>>>>>>> Stashed changes
 
 public class cvm_estimates {
     public static void main(String[] args) {
-        List<String> buffer = new ArrayList<>();
-        int invProbability = 1;
+        long startTime = System.nanoTime();
+        cvmWithInt("random_numbers2");
+        long endTime = System.nanoTime();
+        long totalTime = endTime - startTime;
+        System.out.println(totalTime);
+    }
+    private static void cvmWithInt(String fileName){
+            List<String> buffer = new ArrayList<>();
+            Set<String> set = new HashSet<String>();
+            int invProbability = 1;
 
-        try (BufferedReader br = new BufferedReader(new FileReader("randomnumber2.txt"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                int item = Integer.parseInt(line.trim());
+            try (BufferedReader br = new BufferedReader(new FileReader(fileName + ".txt"))) {
+                String line;
+                while ((line = br.readLine()) != null) {
 
-                if (buffer.contains(Integer.toString(item))) {
-                    if (!check(invProbability)) {
-                        buffer.remove(Integer.toString(item));
-                    }
-                } else {
-                    if (check(invProbability)) {
-                        if (isBufferFull(buffer)) {
-                            invProbability *= 2;
-                            List<String> itemsToRemove = new ArrayList<>();
-
-                            for (String bufferItem : buffer) {
-                                if (!check(invProbability)) {
-                                    itemsToRemove.add(bufferItem);
-                                }
+                    int item = Integer.parseInt(line.trim());
+                    set.add(Integer.toString(item));
+                    if (buffer.contains(Integer.toString(item))) {
+                        if (!check(invProbability)) {
+                            buffer.remove(Integer.toString(item));
+                        }
+                    } else {
+                        if (check(invProbability)) {
+                            if (isBufferFull(buffer)) {
+                                invProbability = getInvProbability(buffer, invProbability);
+                            } else {
+                                buffer.add(Integer.toString(item));
                             }
-
-                            buffer.removeAll(itemsToRemove);
-                        } else {
-                            buffer.add(Integer.toString(item));
                         }
                     }
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+
+            int result = invProbability * buffer.size();
+            System.out.println("Result: " + result);
+            System.out.println("Set: " + set.size());
+        }
+        private static void cvmWithString(String fileName){
+            List<String> buffer = new ArrayList<>();
+            Set<String> set = new HashSet<String>();
+            int invProbability = 1;
+
+            try (BufferedReader br = new BufferedReader(new FileReader(fileName + ".txt"))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+
+                    String trimmed = line.trim();
+                    set.add(trimmed);
+                    if (buffer.contains(trimmed)) {
+                        if (!check(invProbability)) {
+                            buffer.remove(trimmed);
+                        }
+                    } else {
+                        if (check(invProbability)) {
+                            if (isBufferFull(buffer)) {
+                                invProbability = getInvProbability(buffer, invProbability);
+                            } else {
+                                buffer.add(trimmed);
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            int result = invProbability * buffer.size();
+            System.out.println("Result: " + result);
+            System.out.println("Set: " + set.size());
         }
 
+<<<<<<< Updated upstream
         try{
             Scanner scnr = new Scanner(new File("Shakespeare_data.csv"));
 //            File hamlet = new File("");
@@ -57,7 +105,22 @@ public class cvm_estimates {
 
         int result = invProbability * buffer.size();
         System.out.println("Result: " + result);
+=======
+    private static int getInvProbability(List<String> buffer, int invProbability) {
+        invProbability *= 2;
+        List<String> itemsToRemove = new ArrayList<>();
+
+        for (String bufferItem : buffer) {
+            if (!check(2)) {
+                itemsToRemove.add(bufferItem);
+            }
+        }
+
+        buffer.removeAll(itemsToRemove);
+        return invProbability;
+>>>>>>> Stashed changes
     }
+
 
     private static boolean check(int invProbability) {
         Random random = new Random();
@@ -65,7 +128,14 @@ public class cvm_estimates {
     }
 
     private static boolean isBufferFull(List<String> buffer) {
+<<<<<<< Updated upstream
         int bufferSizeLimit = 10;
+=======
+        int bufferSizeLimit = 1000;
+>>>>>>> Stashed changes
         return buffer.size() >= bufferSizeLimit;
     }
+
+
+
 }
